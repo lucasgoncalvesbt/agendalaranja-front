7a01e5c1bc496a44ab8cc09766d4205997760ee3
Lucas Gonçalves <front.end.lb@gmail.com>
2021-09-11 03:05:25 -0300

    fix: arrumando import do React

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Nav from './components/Nav';
+import { AuthContextProvider } from './contexts/AuthContext';
 import Agendamento from './pages/Agendamento';
 import Estacao from './pages/Estacao';
 import Home from './pages/Home';
@@ -12,11 +13,13 @@ function App() {
   return (
     <BrowserRouter>
       <Nav />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/estacao" exact component={Estacao} />
-        <Route path="/agendamento" exact component={Agendamento} />
-      </Switch>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/estacao" exact component={Estacao} />
+          <Route path="/agendamento" exact component={Agendamento} />
+        </Switch>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
